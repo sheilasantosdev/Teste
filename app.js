fcf9cfd3b004b221d01186cf2f5191ed821eb6cd
sheilasantosdev <108371535+sheilasantosdev@users.noreply.github.com>
2022-09-02 14:56:27 -0300

    atualizando css, html e criando js

---
+++ app.js
@@ -0,0 +1,7 @@
+const getCategories = async (id) => {
+    const request = await fetch(https://619e80d71ac52a0017ba43ea.mockapi.io/api/v1/:endpoint);
+    const requestJson = await request.json();
+    return requestJson.results;
+  };
+  
+  export default getMusics;
